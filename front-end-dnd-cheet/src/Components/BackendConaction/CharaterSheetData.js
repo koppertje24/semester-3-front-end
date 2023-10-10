5d0939f0b1df3394d63964221da65eeb9c5b5da7
koppertje24 <32428243+koppertje24@users.noreply.github.com>
2023-10-10 10:35:43 +0200

    get get character trough player.

--- front-end-dnd-cheet/src/Components/BackendConaction/CharaterSheetData.js
+++ front-end-dnd-cheet/src/Components/BackendConaction/CharaterSheetData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function fetchDataById(id) {
     try {
-        const response = await axios.get(`http://localhost:8080/characters/${id}`);  console.log(response);// Adjust the URL to your backend endpoint
+        const response = await axios.get(`http://localhost:8080/players/${id}`);  console.log(response);// Adjust the URL to your backend endpoint
         const data = response.data;
         return data;
     } catch (error) {
