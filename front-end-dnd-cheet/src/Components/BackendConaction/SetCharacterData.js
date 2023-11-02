40f8159cb07f87be2331f2f9039822a7568e2169
laptop-002 <annekoppersdev@gmail.com>
2023-11-02 11:47:23 +0100

    added more cheet data

--- front-end-dnd-cheet/src/Components/BackendConaction/SetCharacterData.js
+++ front-end-dnd-cheet/src/Components/BackendConaction/SetCharacterData.js
@@ -7,16 +7,27 @@ async function SetCharacterData(event, state) {
     const { characterClass } = state;
     const { sheet_id: userId } = state;
 
+    const abilityScores  = [12, 15, 14, 17, 8, 20];
+    const skillProficiency = [true, true, false];
+    const characterLevel = 1;
+
     const characterSheetData = {
         characterName,
-        userId,
         characterClass,
+        abilityScores,
+        skillProficiency,
+        characterLevel,
+        userId,
     };
 
     console.log('player id:', id);
     console.log('sheet id:', userId);
     console.log('CharacterName:', characterName);
     console.log('CharacterClass:', characterClass);
+
+    console.log('AbilityScores:', abilityScores);
+    console.log('SkillProficiency:', skillProficiency);
+    console.log('CharacterLevel:', characterLevel);
 
     const apiUrl = userId ? `http://localhost:8080/players/put/${id}/Character/${userId}` : `http://localhost:8080/players/post/${id}/Character`;
 
